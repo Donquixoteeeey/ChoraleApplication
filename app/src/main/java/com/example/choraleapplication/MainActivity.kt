61c9c26ed99e9e5618fcc392e1ab036b5fc3e097
Donquixoteeeey <imawtin@gmail.com>
2023-12-16 22:28:41 +0800

    First Commit

---
+++ app/src/main/java/com/example/choraleapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.choraleapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
